20e6cb9f04930ac239442a5090ad6d34abd4bd1a
Manu Cornet <manucornet@google.com>
2017-02-28 18:03:39 -0800

    Kill the requested package after each test, and start the requested activity.

--- src/com/android/keynavcheck/KeyNavCheck.java
+++ src/com/android/keynavcheck/KeyNavCheck.java
@@ -42,17 +42,33 @@ public class KeyNavCheck extends CyborgTest {
   private static final int MAX_NUMBER_OF_CYCLABLE_ELEMENTS_AT_TOP_LEVEL = 15;
   private static final int MAX_EXPLORATION_CYCLE_LENGTH = 40;
 
-  public KeyNavCheck() {
+  private final String pkg;
+  private final String activity;
+
+  public KeyNavCheck(String pkg, String activity) {
     super(OPTIONS);
+    this.pkg = pkg;
+    this.activity = activity;
+  }
+
+  @Override
+  public void runTests(CyborgTest testObject) {
+    tearDown();
+    cyborg.pressHome();
+    super.runTests(testObject);
   }
 
   @Override
   public void setUp() {
-    System.err.println("\n\n######################################################################\n");
+    System.err.println("\n\n###################################################################\n");
+    cyborg.runShellCommand("am start -n " + this.pkg + "/" + this.activity);
+    cyborg.onAfterUserInteraction(5000);
   }
 
   @Override
   public void tearDown() {
+    cyborg.runShellCommand("am force-stop " + this.pkg);
+    cyborg.onAfterUserInteraction(2000);
   }
 
   private ViewNode getFocusedNode() {
@@ -114,7 +130,7 @@ public class KeyNavCheck extends CyborgTest {
         numberOfNodesInTopLevelCycle <= MAX_NUMBER_OF_CYCLABLE_ELEMENTS_AT_TOP_LEVEL);
   }
 
-  public void testAllClickableElementsCanBeAccessed() {
+  public void disabledTestAllClickableElementsCanBeAccessed() {
     boolean testPassed = true;
 
     Set<String> visitedNodeIds = new HashSet<>();
@@ -180,6 +196,11 @@ public class KeyNavCheck extends CyborgTest {
   }
 
   public static void main(String[] args) {
-    new KeyNavCheck().init();
+    if (args.length <= 0 || !args[0].contains("/")) {
+      System.err.println("Usage: keynavcheck package/activity");
+      System.exit(-1);
+    }
+    String[] parts = args[0].split("/");
+    new KeyNavCheck(parts[0], parts[1]).init();
   }
 }
